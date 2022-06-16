7a114fefe6a3620357c095d1211cff27e16785e2
hellonayeon <hellonykwon@gmail.com>
2022-06-16 14:09:11 +0900

    add: 채팅 프로그램

---
+++ Nayeon/chat/client/src/dto/type/DtoType.java
@@ -0,0 +1,14 @@
+package dto.type;
+
+public enum DtoType {
+    INIT,
+
+    LOGIN,
+    CREATE_CHAT,
+    ENTER_CHAT, EXIT_CHAT,
+    MESSAGE,
+
+    USER_LIST, CHAT_ROOM_LIST,
+
+    PROGRAM_EXIT
+}
